ff31134630f680f10d085696b871ade62ea6f42f
Zaahiid <zahidrsheikh01@gmail.com>
2024-03-07 19:47:21 +0530

    Added styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
 
   return (
     <>
-    <div className="flex flex-col justify-center items-center mt-20">
-      <h1>Learn about redux and redux-toolkit </h1>
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-4xl my-6 font-semibold text-zinc-600">Learn about redux and redux-toolkit </h1>
       <AddTodo/>
       <Todos/>
       </div>
